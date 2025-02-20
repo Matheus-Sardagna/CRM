5a21ae0e6127aa46d3146ce6fde794ab6741a5ca
Matheus-Sardagna <matheussardagna02@gmail.com>
2025-02-20 12:00:16 -0300

    codand

--- crm/crm_app/views.py
+++ crm/crm_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def clientes(request):
+    return render(request, 'pages/clientes.html')
